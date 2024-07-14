52b0fd7a46644c3b7c7a3755f7b58f56d469affb
yuudi <yuudi@users.noreply.github.com>
2024-07-14 16:30:06 -0400

    ♻️ move three process to individual functions

--- src/routines/star-this-repo.ts
+++ src/routines/star-this-repo.ts
@@ -1,10 +1,5 @@
-import { JobResult, Routine } from "puppilot-routine-base";
-
-const enum StarStatus {
-  Starred,
-  Unstarred,
-  NotSignedIn,
-}
+import { Page } from "puppeteer-core";
+import { JobResult, Routine, Store } from "puppilot-routine-base";
 
 class StarThisRepo extends Routine {
   static displayName = "Star the Puppilot repository on GitHub";
@@ -15,7 +10,7 @@ class StarThisRepo extends Routine {
     '(example routine) Star the "puppilot" repository on GitHub';
 
   public async start(): Promise<JobResult> {
-    // before starting the routine, check if the we already starred the repository
+    // before opening page, check if the we already starred the repository
     const store = await this.getStore();
     const starred = await store.get<boolean>("starred");
     if (starred) {
@@ -25,49 +20,49 @@ class StarThisRepo extends Routine {
       };
     }
 
-    // start the routine
     // create a new page
     const page = await this.getPage();
 
     await page.goto("https://github.com/puppilot-org/puppilot");
+
     // three possible statuses: starred, unstarred, notSignedIn
     // we use three promises to wait for the three possible statuses
-    const starButton$ = page
-      .waitForSelector(".unstarred.BtnGroup > form > button", {
-        visible: true,
-      })
-      .then((button) => ({ button, status: StarStatus.Unstarred as const }));
-    const unstarButton$ = page
-      .waitForSelector(".starred.BtnGroup > form > button", {
-        visible: true,
-      })
-      .then((button) => ({ button, status: StarStatus.Starred as const }));
-    const signInButton$ = page
-      .waitForSelector(".HeaderMenu-link--sign-in", {
-        visible: true,
-      })
-      .then((button) => ({ button, status: StarStatus.NotSignedIn as const }));
-    // wait for any of the three promises to resolve
-    const statusResult = await Promise.any([
-      starButton$,
-      unstarButton$,
-      signInButton$,
+    return Promise.any([
+      this.processNotSignedIn(page),
+      this.processAlreadyStarred(page),
+      this.processUnstarred(page, store),
     ]);
-    // handle the resolved promise
-    if (statusResult.status === StarStatus.NotSignedIn) {
-      return {
-        status: "failed",
-        message: "Not signed in",
-      };
-    }
-    if (statusResult.status === StarStatus.Starred) {
-      return {
-        status: "skipped",
-        message: "already starred",
-      };
-    }
+  }
+
+  private async processNotSignedIn(page: Page) {
+    await page.waitForSelector(".HeaderMenu-link--sign-in", {
+      visible: true,
+    });
+    return {
+      status: "failed",
+      message: "Not signed in",
+    } as const;
+  }
+
+  private async processAlreadyStarred(page: Page) {
+    await page.waitForSelector(".starred.BtnGroup", {
+      visible: true,
+    });
+    return {
+      status: "skipped",
+      message: "Already starred the repository",
+    } as const;
+  }
+
+  private async processUnstarred(page: Page, store: Store) {
+    const starButton = await page.waitForSelector(
+      ".unstarred.BtnGroup > form > button",
+      {
+        visible: true,
+      },
+    );
     // click the star button
-    await statusResult.button?.click();
+    await starButton?.click();
     // wait for the star button to change to already starred
     await page.waitForSelector(".starred.BtnGroup", {
       visible: true,
@@ -78,7 +73,7 @@ class StarThisRepo extends Routine {
     return {
       status: "completed",
       message: "Starred the repository",
-    };
+    } as const;
   }
 }
 
